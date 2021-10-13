5d841512d990f461b69ea9344b26252b5654308e
BhaktiMadiaP <bhakti.m@polynomial.ai>
2021-10-13 12:35:01 +0530

    remove extra component

--- src/app/module/search-property/search-property-routing.module.ts
+++ src/app/module/search-property/search-property-routing.module.ts
@@ -1,8 +1,6 @@
 import { SearchOption2Component } from './search-option2/search-option2.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PropertyListComponent } from './property-list/property-list.component';
-import { SearchComponent } from './search/search.component';
 import { PropertyListOption2Component } from './property-list-option2/property-list-option2.component';
 
 const routes: Routes = [
--- src/app/module/search-property/search-property.module.ts
+++ src/app/module/search-property/search-property.module.ts
@@ -1,11 +1,8 @@
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SearchPropertyRoutingModule } from './search-property-routing.module';
-import { SearchComponent } from './search/search.component';
-import { PropertyListComponent } from './property-list/property-list.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { SearchOption2Component } from './search-option2/search-option2.component';
 import { PropertyListOption2Component } from './property-list-option2/property-list-option2.component';
@@ -13,8 +10,6 @@ import { PropertyListOption2Component } from './property-list-option2/property-l
 
 @NgModule({
   declarations: [
-    SearchComponent,
-    PropertyListComponent,
     SearchOption2Component,
     PropertyListOption2Component
   ],
